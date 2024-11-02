1141df9034da5c1f94515aa38293c78f8d49b3ec
Juan Morales <juanskate326@gmail.com>
2024-11-02 02:15:36 -0500

    cerrar sesion

--- src/app/(pages)/home/components/BodyChat.tsx
+++ src/app/(pages)/home/components/BodyChat.tsx
@@ -1,22 +1,38 @@
 import { useChatStore } from '@/hooks/chat.hook'
+import { FaXmark } from 'react-icons/fa6'
 
 export default function BodyChat() {
 
-    const { contactsSelected } = useChatStore()
+    const { contactsSelected, setContactsSelected } = useChatStore()
+
+    const handleSelectContact = (contact: IConversation) => {
+        const contactFound = contactsSelected.findIndex((contactSelected) => contactSelected.id === contact.id);
+
+        if (contactFound !== -1) {
+            const newContactsSelected = contactsSelected.filter((c, i) => i !== contactFound);
+            setContactsSelected(newContactsSelected);
+        } else {
+            const newContactsSelected = [...contactsSelected, contact];
+            setContactsSelected(newContactsSelected);
+        }
+    }
 
     return (
         <div className='bodyChat overflow-y-auto scrollbar-custom p-4'>
             <div className='flex flex-wrap gap-4 w-full'>
 
                 {contactsSelected.map((contact, index) => (
-                    <div className='p-1 pe-4 rounded-full w-fit border flex gap-4' key={index}>
+                    <div key={index} className='p-1 pe-4 rounded-full w-fit border flex gap-2 hover:scale-105 transition-all duration-300 hover:bg-gray-100'>
                         <img src={`https://ui-avatars.com/api/?name=${contact!.contactName?.replaceAll(" ", "+")}&background=random`} alt="contractor" className='rounded-full' style={{ width: '40px' }} />
-                        <div>
+                        <div >
                             <h1 className='font-bold text-sm'>{contact!.contactName}</h1>
                             <p className='text-sm font-light'>
                                 {contact!.email}
                             </p>
                         </div>
+                        <button onClick={() => handleSelectContact(contact)}>
+                            <FaXmark />
+                        </button>
                     </div>
                 ))}
             </div>
--- src/components/Drawer/DrawerFirst.tsx
+++ src/components/Drawer/DrawerFirst.tsx
@@ -10,7 +10,7 @@ import { IUser } from '@/models/IUser.model';
 import { useChatStore } from '@/hooks/chat.hook';
 
 export default function DrawerFirst() {
-    const { setOnModalTemplate, setMessageType, setTemplateSelected, setMessagesSent } = useChatStore();
+    const { setOnModalTemplate, setMessageType, setTemplateSelected, setMessagesSent, setContactsSelected } = useChatStore();
     const [isMounted, setIsMounted] = useState(false);
     const [tokenDecrypted, setTokenDecrypted] = useState({ userName: '', userEmail: '' });
     const [data, setData] = useState<IUser>();
@@ -88,14 +88,12 @@ export default function DrawerFirst() {
             <div className="drawer-buttons">
                 {filteredMenuItems.map(({ icon, label, route }, idx) => (
                     <Link href={route} key={idx}>
-                        <button className={`w-full flex items-center gap-4 p-3 rounded-lg ${pathname === route ? 'bg-gray-200' : 'bg-gray-100 hover:bg-gray-200'} transition`}>
+                        <div className={`w-full flex items-center gap-4 p-3 rounded-lg ${pathname === route ? 'bg-gray-200' : 'bg-gray-100 hover:bg-gray-200'} transition`}>
                             <div className="flex justify-center items-center bg-white size-10 rounded-full">
-                                <button onClick={() => router.push("/users1")}>
-                                    <DynamicIcon icon={icon} className="text-lg text-gray-500" />
-                                </button>
+                                <DynamicIcon icon={icon} className="text-lg text-gray-500" />
                             </div>
                             <h3 className='font-semibold'>{label}</h3>
-                        </button>
+                        </div>
                     </Link>
                 ))}
             </div>
@@ -106,8 +104,5 @@ export default function DrawerFirst() {
             </div>
         </div>
     );
-}
-function setContactsSelected(arg0: never[]) {
-    throw new Error('Function not implemented.');
 }
 
